8619dd7afc22290e53a26b7831e3137b2f5be37b
koassumo <0770407@gmail.com>
2020-02-03 15:36:45 +0300

    ДЗ 8

--- server/src/main/java/com/geekbrains/chat/server/AuthManager.java
+++ server/src/main/java/com/geekbrains/chat/server/AuthManager.java
@@ -2,4 +2,5 @@ package com.geekbrains.chat.server;
 
 public interface AuthManager {
     String getNicknameByLoginAndPassword(String login, String password);
+    void  changeNickInAuthArray (String oldNick, String newNick);
 }
